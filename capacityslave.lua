9210ad6fb626ef54b817b58bd78956535e5abbaa
Nukise <reyesbros1029@gmail.com>
2025-09-17 22:05:09 -0500

    updated to clear terminal and reset cursorpos

--- capacityslave.lua
+++ capacityslave.lua
@@ -1,6 +1,8 @@
 local modem = peripheral.find("modem")
 local meter = peripheral.wrap("left")
-
+term.clear()
+term.setCursorPos(1,1)
+print("hello i am sending stressometer information on channels 37 & 92")
 while true do
     modem.transmit(37,0,meter.getStressCapacity())
     os.sleep(.1)
