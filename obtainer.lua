620e5c93a731894b0dce59c23e448289fc80b494
Nukise <reyesbros1029@gmail.com>
2025-09-17 21:55:49 -0500

    rewrite for much better format

--- obtainer.lua
+++ obtainer.lua
@@ -1,23 +1,29 @@
 local monitor = peripheral.find("monitor") or error("monitor not found",0)
 local modem = peripheral.find("modem") or error ("modem not found",0)
-local x,y = monitor.getSize()
-local xMiddle = (x/2)
-local yMiddle = (y/2)
-local window = window.create(monitor, ((x/4)+0), ((y/4)+1), (xMiddle+5), yMiddle)
-monitor.clear()
+term.redirect(monitor)
+
+function pos(...) return term.setCursorPos(...) end
+function cls(...) return term.clear() end
+function tCol(...) return term.setTextColor(...) end
+function bCol(...) return term.setBackgroundColor(...) end
+function box(...) return paintutils.drawFilledBox(...) end
+function line(...) return paintutils.drawLine(...) end
 
 function redrawWindow()
-    monitor.setPaletteColor(1,1,2,1)
-    monitor.setBackgroundColor(1)
-    window.setCursorBlink(false)
-    window.setPaletteColor(1,1,1,1)
-    window.setPaletteColor(2,255,255,255)
-    window.setTextColor(2)
-    window.setBackgroundColor(1)
-    window.clear()
+    x,y = monitor.getSize()
+    cls()
+    pos(1,1)
+    box(1,1,x,y,colors.white)
+    box(((x/4)-1),3,(x/1.25),8,colors.lightGray)
+    line(((x/4)-1),4,(x/1.25),4,colors.lightGray)
+    line((x/4),6,(x/1.25),6,colors.lightGray)
+    --line(1,1,3,1,colors.red) 
+    --line(1,2,3,2,colors.black) 
+    --line(1,3,3,3,colors.black) 
+    --line(1,4,3,4,colors.green) 
+    --line(1,5,3,5,colors.green) 
+    term.setCursorBlink(true)
 end
-
-
 
 
 function totalCapacity(capacity)
@@ -44,8 +50,15 @@ end
 
 while true do
     redrawWindow()
-    term.redirect(window)
-    print("Network Total Capacity:"..totalCapacity(capacity))
-    print("Network Capacity Used:"..stressUsed(amount))
-    os.sleep(2)
+    pos((x/4),4)
+    print("Network Capacity Used")
+    pos((x/2.5),5)
+    print(stressUsed(amount),"su")
+    pos((x/4),6)
+    print("Network Total Capacity")
+    pos((x/2.5),7)
+    print(totalCapacity(capacity), "su")
+    os.sleep(10)
 end
+
+
